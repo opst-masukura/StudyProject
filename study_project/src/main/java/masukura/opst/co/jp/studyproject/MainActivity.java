8f78bf7226832316169746eb5334871f09f38f9e
sugitani-hiroki <sugitani.hiroki@opst.co.jp>
2018-03-15 18:02:07 +0900

    課題4仕様に改修

--- study_project/src/main/java/masukura/opst/co/jp/studyproject/MainActivity.java
+++ study_project/src/main/java/masukura/opst/co/jp/studyproject/MainActivity.java
@@ -1,5 +1,7 @@
 package masukura.opst.co.jp.studyproject;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -17,7 +19,17 @@ public class MainActivity extends AppCompatActivity {
 		Button button1 = findViewById(R.id.button1);
 		button1.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View view) {
-				finish();
+				new AlertDialog.Builder(MainActivity.this)
+						.setTitle(R.string.title)
+						.setMessage(R.string.message)
+						.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+								finish();
+							}
+						})
+						.setNegativeButton(R.string.cancel, null)
+						.show();
 			}
 		});
 		Button button2 = findViewById(R.id.button2);
